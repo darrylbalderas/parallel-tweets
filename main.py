5dd21a3e8e183d5564bccffe90d2c57b168f03d6
darrylbalderas <drrylbalderas@gmail.com>
2020-12-20 15:23:31 -0800

    Add multi processing for gathering tweets

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-import time
-
-from tweet import TwitterApi, TweetProcessor
+from tweet import TwitterProcessor
+import tweepy
+import os
 
 
 def main():
+    consumer_key = os.environ.get("CONSUMER_KEY")
+    consumer_secret = os.environ.get("CONSUMER_SECRET")
+    access_token = os.environ.get("ACCESS_TOKEN")
+    access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
+    # Authenticate to Twitter
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
 
-    api = TwitterApi(TweetProcessor())
+    processor = TwitterProcessor(tweepy.API(auth))
 
-    results = api.proess_tweets()
+    processor.pull_trending_tweets()
 
+    results = processor.get_results()
+
+    # TODO: Store results in Dynamo DB
     print(results)
 
 
--- tweet.py
+++ tweet.py
@@ -1,18 +1,73 @@
 from multiprocessing import Process, Queue
-import os
 import tweepy
 
 
 class TweetProcessor:
-    def __init__(self, num_workers=4) -> None:
+    def __init__(self, api, num_workers=4) -> None:
         self.tweets = Queue()
         self.results = Queue()
         self.num_workers = num_workers
+        self.search_size_per_topic = 10
+        self.api = api
 
-    def add_tweet(self, tweet) -> None:
-        self.tweets.put(tweet)
 
-    def transform(self) -> None:
+class TwitterProcessor:
+    def __init__(self, api: tweepy.API, num_workers=3) -> None:
+        self.api = api
+        self.acceptable_countries = {'US'}
+        self.acceptable_towns = {'San Francisco'}
+        self.tweets = Queue()
+        self.results = Queue()
+        self.num_workers = num_workers
+        self.search_size_per_topic = 10
+
+    def apply_trend_filter(self, trend):
+        country_code = trend['countryCode']
+        town_name = trend['name']
+        is_country_code = country_code in self.acceptable_countries
+        is_town = town_name in self.acceptable_towns
+        return is_country_code and is_town
+
+    def pull_trending_tweets(self, search_size=10, num_topics=5):
+        country_trends = filter(self.apply_trend_filter, self.api.trends_available())
+        for country_trend in country_trends:
+            woeid = country_trend['woeid']
+            trending_topics = sorted(self.api.trends_place(woeid)[0]['trends'],
+                                     key=lambda x: x['tweet_volume']
+                                     if x['tweet_volume'] else float('-inf'),
+                                     reverse=True)
+            processes = []
+            slices = len(trending_topics) // self.num_workers
+            for i in range(self.num_workers):
+                start = i * slices
+                end = i * slices + slices
+                delta = abs(end - len(trending_topics))
+                if delta < slices:
+                    end += delta
+                processes.append(
+                    Process(
+                        target=self.store_tweets,
+                        args=(trending_topics, start, end),
+                    ))
+
+            for p in processes:
+                p.start()
+
+            for p in processes:
+                p.join()
+
+    def store_tweets(self, topics, start, end) -> None:
+        for topic in topics[start:end]:
+            for tweet in self.api.search(q=topic['name'],
+                                         result_type='recent',
+                                         count=10):
+                # for tweet in tweepy.Cursor(self.api.search,
+                #                            q=topic['name'],
+                #                            result_type='recent').items(
+                #                                self.search_size_per_topic):
+                self.tweets.put(tweet)
+
+    def process_tweets(self) -> None:
         while not self.tweets.empty():
             tweet = self.tweets.get()
             self.results.put(
@@ -24,10 +79,10 @@ class TweetProcessor:
                      user_id=tweet.user.id,
                      user_screen_name=tweet.user.screen_name))
 
-    def run(self):
+    def get_results(self):
         processes = [
             Process(
-                target=self.transform,
+                target=self.process_tweets,
                 args=(),
             ) for _ in range(self.num_workers)
         ]
@@ -38,49 +93,7 @@ class TweetProcessor:
         for p in processes:
             p.join()
 
-    def output(self):
         results = []
         while not self.results.empty():
             results.append(self.results.get())
         return results
-
-
-class TwitterApi:
-    def __init__(self, tweet_processor: TweetProcessor) -> None:
-        consumer_key = os.environ.get("CONSUMER_KEY")
-        consumer_secret = os.environ.get("CONSUMER_SECRET")
-        access_token = os.environ.get("ACCESS_TOKEN")
-        access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
-        # Authenticate to Twitter
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        auth.set_access_token(access_token, access_token_secret)
-        # Create API object
-        self.api = tweepy.API(auth)
-        self.tweet_processor = tweet_processor
-        self.acceptable_countries = {'US'}
-        self.acceptable_towns = {'San Francisco'}
-
-    def apply_trend_filter(self, trend):
-        country_code = trend['countryCode']
-        town_name = trend['name']
-        is_country_code = country_code in self.acceptable_countries
-        is_town = town_name in self.acceptable_towns
-        return is_country_code and is_town
-
-    def proess_tweets(self, search_size=10, num_topics=5):
-        country_trends = filter(self.apply_trend_filter, self.api.trends_available())
-        for country_trend in country_trends:
-            woeid = country_trend['woeid']
-            trending_topics = sorted(self.api.trends_place(woeid)[0]['trends'],
-                                     key=lambda x: x['tweet_volume']
-                                     if x['tweet_volume'] else float('-inf'),
-                                     reverse=True)
-            # Sort by tweet_volume # get 3 top trends
-            for topic in trending_topics[:num_topics]:
-                for tweet in tweepy.Cursor(self.api.search,
-                                           q=topic['name'],
-                                           result_type='recent').items(search_size):
-                    self.tweet_processor.add_tweet(tweet)
-
-        self.tweet_processor.run()
-        return self.tweet_processor.output()
